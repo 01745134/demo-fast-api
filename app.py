572c60ae245298940e2a98db5e6396d7bfe97de9
01745134 <A01745134@tec.mx>
2024-04-08 16:42:30 -0600

    :tada: Primer cambio
    Creación de una API

---
+++ app.py
@@ -0,0 +1,23 @@
+import fastapi
+from fastapi.middleware.cors import CORSMiddleware
+
+app = fastapi.FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+@app.get('/')
+def hello_world():
+    return {'message': 'Hello, World!'}
+
+@app.get('/spanish/')
+def hello_spanish():
+    return {'message': 'Hola, Mundo!'}
+
+@app.get('/{name}')
+def hello_name(name: str):
+    return {'message': f'Hello {name}'}
